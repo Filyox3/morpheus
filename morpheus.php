26174c3e6b5ecacc4bb74762a63af62dd082b41e
Filip Angelov <filyox3@gmail.com>
2017-03-20 15:16:41 +0200

    first commit

---
+++ morpheus.php
@@ -0,0 +1,9 @@
+<?php
+/**
+* Plugin Name: Morpheus
+* Plugin URI: https://www.elegantthemes.com/
+* Description: A custom music review plugin built for example.
+* Version: 1.0
+* Author: Andy Leverenz
+* Author URI: http://justalever.com/
+**/
